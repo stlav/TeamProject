8b63fb8df8de2599b0685f96e21b9db1adb734dd
IvanOA <juzippee@mail.ru>
2016-03-20 18:51:09 +0300

    https://github.com/stlav/TeamProject.git

---
+++ TeamProject/Orders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamProject
+{
+    class Orders
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public int CustomerId { get; set; }
+        public double Price { get; set; }
+        public string Type { get; set; }
+        public int PerformerId { get; set; }
+    }
+}
